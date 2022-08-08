536e832bccb7e732c962fe2c3e0bcc9b1040afdc
hyunmin <choihm9903@naver.com>
2022-08-09 00:22:32 +0900

    feat: 2-players

--- app/src/main/java/com/example/som/QuestionActivity.kt
+++ app/src/main/java/com/example/som/QuestionActivity.kt
@@ -1,24 +1,19 @@
 package com.example.som
 
 import android.content.DialogInterface
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.facebook.stetho.okhttp3.StethoInterceptor
 import kotlinx.android.synthetic.main.activity_question.*
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Math.abs
 
 class QuestionActivity : AppCompatActivity() {
-
-    lateinit var service: RetrofitService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +24,9 @@ class QuestionActivity : AppCompatActivity() {
         var players: ArrayList<TextView> = ArrayList()
         val kCategory = intent.getStringExtra("kcategory")
         val eCategory = intent.getStringExtra("ecategory")
+        var turn = true
+
         category.text = "카테고리 - " + kCategory
-        createRetrofit()
 
         for (i in 0..SIZE) {
             players.add(findViewById(getResources().getIdentifier("board" + i, "id", packageName)))
@@ -41,23 +37,53 @@ class QuestionActivity : AppCompatActivity() {
 
             for ((index,item) in arr.withIndex()) {
                 if (item!=0 && index!=0) {
-                    val ind = if (index == 28) 23 else index
-                    players[ind].setOnClickListener {
-                        var idx = getIndex(ind, num)
-                        arr[idx] += item
-                        arr[ind] = 0
-                        drawGame(arr)
+                    players[index].setOnClickListener {
+                        var idx = getIndex(index, num)
+
+                        if (turn && item > 0) {
+                            if (arr[idx] < 0)
+                                arr[idx] = item
+                            else
+                                arr[idx] += item
+                            arr[index] = 0
+                            drawGame(arr)
+                            players[index].setOnClickListener(null)
+                            turn = !turn
+                        }
+                        if (!turn && item < 0) {
+                            if (arr[idx] > 0)
+                                arr[idx] = item
+                            else
+                                arr[idx] -= item
+                            arr[index] = 0
+                            drawGame(arr)
+                            players[index].setOnClickListener(null)
+                            turn = !turn
+                        }
                     }
                 }
             }
 
             start.setOnClickListener {
-                if (checkBoard(arr) && num != -1) {
-                    arr[num] += 1
+                if (num != -1) {
+                    if (turn) {
+                        if (arr[num] < 0)
+                            arr[num] = 1
+                        else
+                            arr[num] += 1
+                    }
+                    else {
+                        if (arr[num] > 0)
+                            arr[num] = -1
+                        else
+                            arr[num] -= 1
+                    }
                     drawGame(arr)
+                    start.setOnClickListener(null)
+                    turn = !turn
                 }
             }
-            changeQuestion(eCategory!!)
+//            changeQuestion(eCategory!!)
         }
     }
 
@@ -129,7 +155,11 @@ class QuestionActivity : AppCompatActivity() {
                 var player: TextView = findViewById(getResources().getIdentifier("board" + index, "id", packageName))
 
                 if (item!=0) {
-                    val drawable = resources.getIdentifier("player_$item", "drawable", packageName)
+                    var drawable: Int
+                    if (item > 0)
+                        drawable = resources.getIdentifier("player_$item", "drawable", packageName)
+                    else
+                        drawable = resources.getIdentifier(String.format("player_%02d", abs(item)),"drawable", packageName)
                     player.setBackgroundResource(drawable)
                 }
                 else
@@ -168,25 +198,5 @@ class QuestionActivity : AppCompatActivity() {
                 Toast.makeText(this@QuestionActivity, "서버 오류", Toast.LENGTH_LONG).show()
             }
         })
-    }
-
-    fun createRetrofit() {
-        val header = Interceptor {
-            val original = it.request()
-            it.proceed(original)
-        }
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(header)
-            .addNetworkInterceptor(StethoInterceptor())
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8000/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-
-        service = retrofit.create(RetrofitService::class.java)
     }
 }
