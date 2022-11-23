41f872c1866b7ab34949733fefae6cb7a850a0a5
hyunmin <choihm9903@naver.com>
2022-11-23 22:11:46 +0900

    delete: API login

--- app/src/main/java/com/smu/som/RetrofitService.kt
+++ app/src/main/java/com/smu/som/RetrofitService.kt
@@ -4,21 +4,6 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface RetrofitService {
-    @POST("user/signup/")
-    @FormUrlEncoded
-    fun register(
-        @Field("username") username: String,
-        @Field("password") password: String
-    ): Call<User>
-
-
-    @POST("user/login/")
-    @FormUrlEncoded
-    fun login(
-        @Field("username") username: String,
-        @Field("password") password: String
-    ): Call<User>
-
     @GET("question/{category}/")
     fun getQuestion(
         @Path("category") category: String
