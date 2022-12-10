5e79b961b73c8ebe56b71a7b0dac65115ee7601b
hyunmin <choihm9903@naver.com>
2022-12-10 22:03:18 +0900

    delete: choose category

--- app/src/main/java/com/smu/som/MainActivity.kt
+++ app/src/main/java/com/smu/som/MainActivity.kt
@@ -1,24 +1,19 @@
 package com.smu.som
 
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import com.kakao.sdk.user.UserApiClient
 import com.kakao.sdk.user.model.AgeRange
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val categoryArray = arrayOf("연인", "부부", "부모자녀") // 리스트에 들어갈 Array
-        val categoryMap = hashMapOf("연인" to "COUPLE", "부부" to "MARRIED", "부모자녀" to "PARENT")
-
 
         UserApiClient.instance.me { user, error ->
             val ageRange = user?.kakaoAccount?.ageRange
